e64db142c61f716d52f8e4b592f3d29101e8a88d
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-01-23 15:49:20 -0800

    Started to add the function

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let num1 = prompt(`Input number 1`)
+let sign = prompt(`Input a arithmetic sign`)
+let num2 = prompt(`Input number 2`)
+
+if (num1 = 0 || isNaN(num1)){
+    alert(`Number 1 is Not a Compatible Number`)
+}
+
+
+
+
+
+
+console.log(num1);
+console.log(num2);
+console.log(sign);

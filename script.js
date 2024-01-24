e6c63d2d34916041c7f2c6c33392f0e31c89ffb9
Dinoplant <143848581+Dinoplant@users.noreply.github.com>
2024-01-24 14:50:19 -0800

    Finished it

--- script.js
+++ script.js
@@ -3,32 +3,38 @@ let num1 = prompt(`Input number 1`);
 let sign = prompt(`Input a arithmetic sign`);
 let num2 = prompt(`Input number 2`);
 
-if (num1 = 0 || isNaN(num1)){
+
+if (num1 == 0 || isNaN(num1)){
     alert(`Number 1 is Not a Compatible Number`)
 }
 
-if (num2 = 0 || isNaN(num2)){
+if (num2 == 0 || isNaN(num2)){
     alert(`Number 2 is Not a Compatible Number`)
 }
 
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
 if (sign == `+` ){
-    let sum = (num1 + num2);
+     sum = num1 + num2;
     alert(sum);
     console.log (sum);
 }
 else if (sign == `-` ){
-    alert(num1 + num2)
-}
-else if (sign == `*` ){
-    alert(num1 + num2)
-}
-else if (sign == `/` ){
-    alert(num1 + num2)
-}
-else if (sign == `x` || sign == `X` ){
-    let sum = (num1 * num2);
+    sum = num1 - num2;
     alert(sum);
     console.log (sum);
+}
+else if (sign == `*` || sign == `x` || sign == `X` ){
+    sum = num1 * num2;
+    alert(sum);
+    console.log (sum);
+}
+else if (sign == `/` ){
+    sum = num1 / num2;
+    alert(sum);
+    console.log (sum);
+    document.getElementById(`answer`).innerHTML = sum;
 }
 else {
     alert(`The Sign is not a Compatible Sign`)
@@ -37,4 +43,5 @@ else {
 console.log(num1);
 console.log(num2);
 console.log(sign);
-console.log(sum);
+
+document.getElementById(`answer`).innerHTML = sum;

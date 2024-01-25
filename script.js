30dff97c46ec41b7894d84e3e442b3c4679a77f3
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-01-24 21:34:45 -0800

    added javascript to change text

--- script.js
+++ script.js
@@ -4,37 +4,40 @@ let sign = prompt(`Input a arithmetic sign`);
 let num2 = prompt(`Input number 2`);
 
 
-if (num1 == 0 || isNaN(num1)){
+
+if (num1 == 0 || isNaN(num1)) {
     alert(`Number 1 is Not a Compatible Number`)
 }
 
-if (num2 == 0 || isNaN(num2)){
+if (num2 == 0 || isNaN(num2)) {
     alert(`Number 2 is Not a Compatible Number`)
 }
 
 num1 = parseFloat(num1);
 num2 = parseFloat(num2);
 
-if (sign == `+` ){
-     sum = num1 + num2;
+if (sign == `+`) {
+    sum = num1 + num2;
     alert(sum);
-    console.log (sum);
+    console.log(sum);
+
+    let text = document.getElementById(`answer`).innerHTML;
+    document.getElementById(`answer`).innerHTML = text.replace(`poof`, sum)
 }
-else if (sign == `-` ){
+else if (sign == `-`) {
     sum = num1 - num2;
     alert(sum);
-    console.log (sum);
+    console.log(sum);
 }
-else if (sign == `*` || sign == `x` || sign == `X` ){
+else if (sign == `*` || sign == `x` || sign == `X`) {
     sum = num1 * num2;
     alert(sum);
-    console.log (sum);
+    console.log(sum);
 }
-else if (sign == `/` ){
+else if (sign == `/`) {
     sum = num1 / num2;
     alert(sum);
-    console.log (sum);
-    document.getElementById(`answer`).innerHTML = sum;
+    console.log(sum);
 }
 else {
     alert(`The Sign is not a Compatible Sign`)
@@ -44,4 +47,4 @@ console.log(num1);
 console.log(num2);
 console.log(sign);
 
-document.getElementById(`answer`).innerHTML = sum;
+
